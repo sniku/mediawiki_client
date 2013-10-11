0581c88211301141862a98bc377872f34b4fb523
Pawel Suwala <pawel@solidequity.se>
2013-10-11 14:23:08 +0200

    added support for editing articles added support for directly going to article added support for chosing the editor

--- mediawiki_client.py
+++ mediawiki_client.py
@@ -1,54 +1,78 @@
 #!/usr/bin/python
+
 from BeautifulSoup import BeautifulSoup
 import base64
 from cmd import Cmd
 import re
-import sys
 import os
 from subprocess import call
 import urllib
-import urllib2
 import urlparse
 import settings
 import tempfile
+import twill
 
-
-EDITOR = os.environ.get('EDITOR','vim')
+EDITOR = settings.FORCE_EDITOR or os.environ.get('EDITOR', 'vim')
 
 class MediaWikiEditor(object):
 
     def open_article(self, initial_content):
-        with tempfile.NamedTemporaryFile(suffix=".tmp") as tmpfile:
+
+        edited_content = ''
+
+        with tempfile.NamedTemporaryFile(suffix=".tmp", delete=False) as tmpfile:
             tmpfile.write(initial_content)
             tmpfile.flush()
             call([EDITOR, tmpfile.name])
-            edited_content = open(tmpfile.name).read()
+            tmpfile.flush()
+            tmpfile.close()
 
-            if edited_content != initial_content:
-                pass
-                #TODO: should save the edited article
+            edited_file = open(tmpfile.name)
+            edited_content = edited_file.read()
+            edited_file.close()
+
+
+        return edited_content, initial_content
+
 
 class MediaWikiBrowser(object):
     """
     This class emulates a browser. It defines several helper classes for extracting/saving content.
     """
 
-    def openurl(self, url):
-        request = urllib2.Request(url)
+    def __init__(self):
+        self.twill_browser = twill.get_browser()
 
         # Handle HTTP authentication
         if settings.HTTP_AUTH_USERNAME and settings.HTTP_AUTH_PASSWORD:
             base64string = base64.encodestring('%s:%s' % (settings.HTTP_AUTH_USERNAME, settings.HTTP_AUTH_PASSWORD)).replace('\n', '')
-            request.add_header("Authorization", "Basic %s" % base64string)
+            #twill_browser.addheaders( ("Authorization", "Basic %s" % base64string) )
+            self.twill_browser._browser.addheaders += [("Authorization", "Basic %s" % base64string)]
 
-        result = urllib2.urlopen(request)
-        html_content = result.read()
-        return html_content
+        self.twill_browser.go(settings.MEDIAWIKI_URL)
+
+    def save_article(self, url, new_content):
+        self.twill_browser.go(url)
+        self.twill_browser._browser.select_form('editform')
+
+        twill.commands.formvalue('editform', 'wpTextbox1', new_content)
+
+        self.twill_browser.submit()
+
+        #TODO: validate settings and print error messages
+
+    def openurl(self, url):
+        self.twill_browser.go(url)
+        return self.twill_browser.result.page
+
 
     def get_page_content(self, url):
-        html = self.openurl(url)
-        soup = BeautifulSoup(html)
-        return soup.find('textarea').text
+        self.twill_browser.go(url)
+
+        form = self.twill_browser.get_form('editform')
+        content = self.twill_browser.get_form_field(form, 'wpTextbox1')._value
+        return content
+
 
     def _parse_search_results(self, html):
         soup = BeautifulSoup(html)
@@ -82,11 +106,6 @@ class MediaWikiBrowser(object):
         url = urlparse.urljoin(settings.MEDIAWIKI_URL, '/index.php?go=Go&search='+urllib.quote_plus(keyword) )
         html = self.openurl(url)
         search_results = self._parse_search_results(html)
-        if not search_results:
-            print u'No results for "%s"'% keyword
-        else:
-            for result in search_results:
-                print result['index'], result['title'], '\n\t', result['match']
         return search_results
 
 
@@ -100,39 +119,58 @@ class MediaWikiInteractiveCommands(Cmd):
         self.prompt = '\nWiki command: '
         self.browser = MediaWikiBrowser()
         self.editor = MediaWikiEditor()
+        self.last_search_results = []
+        self.last_search_query = ''
 
 
-    def do_search(self, keyword):
+    def do_search(self, keyword, quiet=False):
         """ Search for the keyword """
-        print 'Searching for', keyword
-        self.last_results = self.browser.search(keyword)
+        self.last_search_query = keyword
+        self.last_search_results = self.browser.search(self.last_search_query )
+        if not quiet:
+            print 'Searching for', self.last_search_query
+            self.display_search_list()
 
+    def display_search_list(self):
+        """ Display the last search result """
+        if not self.last_search_results:
+            print u'No results for "%s"'% self.last_search_query
+        else:
+            for result in self.last_search_results:
+                print result['index'], result['title'], '\n\t', result['match']
 
-    def do_display_article(self, url):
+    def do_go(self, page_name):
+        """ go to specified page """
+        url = urlparse.urljoin(settings.MEDIAWIKI_URL, '/index.php?action=edit&title=' + urllib.quote_plus(page_name) )
+        self.display_article(url)
+
+    def display_article(self, url):
+        """ Display the last search result """
         page = self.browser.get_page_content(url)
         # print page
-        self.editor.open_article(page)
+        new_content, old_content = self.editor.open_article(page)
+
+        if old_content != new_content:
+            self.browser.save_article(url, new_content)
+
+
 
     def do_display_search_result(self, index):
-        print 'Displaying', index
+        """ displays the article specified by the index in the search list  """
         try:
             index = int(index) - 1 # we enumerated starting on 1
-            hit = self.last_results[index]
+            hit = self.last_search_results[index]
         except IndexError:
             print 'Wrong index - try again'
         else:
+            print 'Displaying', hit['title']
+
             url = urlparse.urljoin(settings.MEDIAWIKI_URL, '/index.php?action=edit&title=' + urllib.quote_plus(hit['title']) )
-            self.do_display_article(url)
-
-
-    def edit(self, url):
-        pass
-
-    def save(self, content, url):
-        pass
+            self.display_article(url)
 
     def do_EOF(self, line):
-        print 'bye'
+        """ quit """
+        print '\nbye'
         return True
 
     def precmd(self, line):
--- settings.py
+++ settings.py
@@ -1,7 +1,15 @@
-MEDIAWIKI_URL = 'http://en.wikipedia.org/' # remember to include the protocol. example: https://wiki.example.com/
+
+# URL to your wiki instalation
+MEDIAWIKI_URL = 'https://wiki.example.com/' # remember to include the protocol. example: https://wiki.example.com/
+
+# HTTP authentication
+# leave empty if the page is public
 HTTP_AUTH_USERNAME = ''
 HTTP_AUTH_PASSWORD = ''
 
+# leave empty if you want your default $EDITOR
+# or choose vim, nano, pico, etc
+FORCE_EDITOR = ''
 
 try:
     from local_settings import *
